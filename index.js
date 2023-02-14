802192d472cec4e9d01cd03342fa154ce972ad02
ESLAMARAF <117894544+ESLAMARAF@users.noreply.github.com>
2023-02-14 16:28:21 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+// JavaScript Coding Start From Here...
+
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector("body").classList.add("display");
+  }, 4000);
+};
+
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+  document.querySelector(".container").classList.toggle("change");
+});
+
+document.querySelector(".scroll-btn").addEventListener("click", () => {
+  document.querySelector("html").style.scrollBehavior = "smooth"
+  setTimeout(() => {
+    document.querySelector("html").style.scrollBehavior = "unset";
+  }, 1000);
+});
+
+// JavaScript Coding END Here...
